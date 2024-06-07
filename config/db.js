8090b87f3e84689351d6c851796517cc7383c484
MeriemFtahallah <67705552+MeriemFat@users.noreply.github.com>
2024-06-07 11:22:53 +0100

    meriem-host

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const database = 'Attakafoulia';
-    const port = '27017';  // le port MongoDB par défaut
+    //const database = 'Attakafoulia';
+   // const port = '27017';  // le port MongoDB par défaut
 
     try {
-        await mongoose.connect(`mongodb://127.0.0.1:${port}/${database}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: false
+        await mongoose.connect(`mongodb+srv://menm4671:1234@cluster0.5vefyxj.mongodb.net/Attakafoulia?retryWrites=true&w=majority&appName=Cluster0`, {
         });
-
         console.log("Database connected successfully");
     } catch (err) {
         console.error(err.message);
